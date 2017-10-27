cd3ea396571d25e605ca636767c0c6ab9e7a4352
esilvac <esilva1995@gmail.com>
2017-10-27 17:01:33 -0500

    first model created

---
+++ db/migrate/20171027212820_create_customers.rb
@@ -0,0 +1,13 @@
+class CreateCustomers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :customers, id: false do |t|
+      t.primary_key :customer_id
+      t.string :first_name, limit: 25, null: false
+      t.string :last_name, limit: 25, null: false
+      t.string :email, limit: 45, null: false
+      t.string :address, limit: 50, null: false 
+      t.integer :customer_type, null: false 
+      t.timestamps
+    end
+  end
+end
