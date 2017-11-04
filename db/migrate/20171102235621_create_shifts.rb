70ddb768eaede615f45527c6e585985908c22b7d
omontoy <omb0320@gmail.com>
2017-11-03 19:31:58 -0500

    Cambia t.date a t.time en horas de entrada y salida

--- db/migrate/20171102235621_create_shifts.rb
+++ db/migrate/20171102235621_create_shifts.rb
@@ -2,8 +2,8 @@ class CreateShifts < ActiveRecord::Migration[5.1]
   def change
     create_table :shifts, id: false do |t|
       t.primary_key :shift_id
-      t.date :entry_time
-      t.date :departure_time      
+      t.time :entry_time
+      t.time :departure_time      
     end
   end
 end
